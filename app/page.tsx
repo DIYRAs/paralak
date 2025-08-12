a509a86417e258d133da5b886c978b73f238b983
diyra418@gmail.com <diyra418@gmail.com>
2025-08-13 06:56:16 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,85 @@
-import Image from "next/image";
+'use client'
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const scrollTrigger1 = useRef(null)
+  const moveToRight1 = useRef(null)
+  const moveToRight2 = useRef(null)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const moveToLeft1 = useRef(null)
+  const moveToLeft2 = useRef(null)
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useEffect(() => {
+    gsap.to(moveToRight2.current, {
+      scrollTrigger: {
+        trigger: scrollTrigger1.current,
+        scrub: 1,
+        markers: true
+      },
+      translateX: 1000
+    })
+    gsap.to(moveToRight1.current, {
+      scrollTrigger: {
+        trigger: scrollTrigger1.current,
+        scrub: 1,
+        markers: true
+      },
+      translateX: 100
+    })
+
+    gsap.to(moveToLeft2.current, {
+      scrollTrigger: {
+        trigger: scrollTrigger1.current,
+        scrub: 1,
+        markers: true
+      },
+      translateX: -1000
+    })
+    gsap.to(moveToLeft1.current, {
+      scrollTrigger: {
+        trigger: scrollTrigger1.current,
+        scrub: 1,
+        markers: true
+      },
+      translateX: -100
+    })
+  }, [])
+
+  return (
+    <>
+      <div className="relative flex flex-col items-center justify-end w-full h-screen bg-zinc-700">
+
+        <div
+          className="relative w-full h-full bg-zinc-300">
+
+          <div
+            ref={moveToRight2}
+            className="absolute bottom-0 right-0 w-2/4 h-2/3 bg-zinc-600" />
+          <div
+            ref={moveToLeft2}
+            className="absolute bottom-0 left-0 w-2/4 h-2/3 bg-zinc-600" />
+
+          <div
+            ref={moveToRight1}
+            className="absolute bottom-0 right-0 w-2/5 h-24 bg-zinc-800" />
+          <div
+            ref={moveToLeft1}
+            className="absolute bottom-0 left-0 w-2/5 h-24 bg-zinc-800" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div
+          className="w-full h-56 bg-zinc-500" />
+      </div>
+
+      <div
+        ref={scrollTrigger1}
+        className="w-full h-screen bg-zinc-800">
+
+      </div>
+    </>
   );
 }
